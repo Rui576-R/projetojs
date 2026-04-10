1c318ca10939d6d18f36a764b242742a0d99d3c9
ruiaparecido <82909004+Rui576-R@users.noreply.github.com>
2026-04-09 21:50:49 -0300

    Update script.js

--- aula12ex/ex0015/script.js
+++ aula12ex/ex0015/script.js
@@ -3,7 +3,7 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    var hora = 23
+    //var hora = 23
     msg.innerHTML = `Agora são ${hora} horas.`
     if(hora >= 0 && hora < 12) {
         // BOM DIA
